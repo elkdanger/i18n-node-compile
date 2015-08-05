9264cb78e0648d0448c5233e30845f8e9badb07d
Steve Hobbs <elkdanger@gmail.com>
2015-08-05 16:29:36 +0100

    Default options setup

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var compiler = module.exports = function() {	
+var path = require('path');
+var glob = require('glob');
+var _ = require('underscore')._;
+
+var defaultOptions = {
+	output: './locale',
+	match: './locale/**/*.js',
+	locales: ['en']
+};
+
+var compiler = module.exports = function() {
+	
+	var self = this;
+		
+	
+		
 };
